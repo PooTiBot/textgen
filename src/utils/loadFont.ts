7c2e107e57f50ac887ec0e27edac4042e8614f7d
PooTiBot <undigundiii@gmail.com>
2026-08-13 12:59:05 +0300

    Fix Cyrillic font loading

--- src/utils/loadFont.ts
+++ src/utils/loadFont.ts
@@ -1,7 +1,9 @@
-import * as opentype from "opentype.js";
+import opentype from "opentype.js";
+
+const FONT_URL = "/fonts/TextGen3D-Sans.ttf";
 
 export async function loadFont() {
-    const response = await fetch("/fonts/ARIAL.TTF");
+    const response = await fetch(FONT_URL);
 
     if (!response.ok) {
         throw new Error(`Не удалось загрузить шрифт: ${response.status}`);
